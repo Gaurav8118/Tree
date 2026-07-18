53873e89de27eeafbc863ae44fbc00d5f671724e
Gaurav Gusani <gauravgusani811@gmail.com>
2026-07-18 19:04:40 +0530

    Time: 8 ms (61.95%) | Memory: 16.2 MB (32.93%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -0,0 +1,26 @@
+
+
+
+class Solution:
+    def generateTrees(self, n):
+        if n == 0:
+            return []
+        
+        def build(start, end):
+            if start > end:
+                return [None]
+            
+            all_trees = []
+            for root_val in range(start, end + 1):
+                left_trees = build(start, root_val - 1)
+                right_trees = build(root_val + 1, end)
+                
+                for l in left_trees:
+                    for r in right_trees:
+                        root = TreeNode(root_val)
+                        root.left = l
+                        root.right = r
+                        all_trees.append(root)
+            return all_trees
+        
+        return build(1, n)
